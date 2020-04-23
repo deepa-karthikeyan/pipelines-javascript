4bd6cfa61b1cacef4dbd1d7fbe080addb758885a
deepa karthikeyan <59433225+deepa-karthikeyan@users.noreply.github.com>
2020-04-23 14:26:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 // Constants
 const PORT = 3000;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Welcome to Azure DevOps!\n');
+app.get("/", (req, res) => {
+  res.send("Welcome to Azure DevOps!\n");
 });
 
-app.get('/html', (req, res) => {
+app.get("/html", (req, res) => {
   res.sendFile("sample_page.html", {
-    root: __dirname
+    root: __dirname,
+  });
 });
 
-var port = process.env.PORT||PORT;
+var port = process.env.PORT || PORT;
 app.listen(port);
 console.log(`Running on http://${HOST}:${PORT}`);
